fe903d6031f89ecdd595631d809bda61c27b45e8
Saisri Gandla <gandlasaisri895@gmail.com>
2026-03-13 20:52:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+(function(d, t) {
+      var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
+      v.onload = function() {
+        window.voiceflow.chat.load({
+          verify: { projectID: '69afde40ba7200e1081bed6b' },
+          url: 'https://general-runtime.voiceflow.com',
+          versionID: 'production',
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
+          }
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs";
+      v.type = "text/javascript";
+      s.parentNode.insertBefore(v, s);
+  })(document, 'script');
